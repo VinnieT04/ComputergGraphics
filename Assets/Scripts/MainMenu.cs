ecdd60d8138e99eea77c3d3e301a7856b266d077
Violeta Torres <cvioletats@gmail.com>
2026-05-23 14:47:28 +0800

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Options()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
